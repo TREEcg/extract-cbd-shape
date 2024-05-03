6972f1b96de810309406e9e15b395d24a33f37e7
Pieter Colpaert <pieter.colpaert@ugent.be>
2024-05-03 15:28:47 +0200

    Added option to also use LDES or Collection IRI on cli

--- bin/mermaid.ts
+++ bin/mermaid.ts
@@ -5,20 +5,22 @@ import { DataFactory } from "rdf-data-factory";
 import rdfDereference from "rdf-dereference";
 import fs from "fs/promises";
 import * as process from 'process';
+import { Term } from "rdf-js";
 const df = new DataFactory();
 
 // Check if at least one command line argument is provided
 if (process.argv.length <= 2) {
-  console.error('Please provide an IRI to a dereferenceable SHACL NodeShape');
+  console.error('Please provide an IRI to a dereferenceable SHACL NodeShape or an LDES or tree:Collection with with tree:shape property in it');
   process.exit(1); // Exit with an error code
 }
 
-const iri = process.argv[2];
+let iri = process.argv[2];
 
 async function main () {
     let df = new DataFactory();
     let shapeStore = RdfStore.createDefault();
     let shapesGraph: ShapesGraph;
+    let shapeTerm: Term = df.namedNode(iri);
     let readStream = (
       await rdfDereference.dereference(iri, {
         localFiles: true,
@@ -28,9 +30,30 @@ async function main () {
     await new Promise((resolve, reject) => {
       shapeStore.import(readStream).on("end", resolve).on("error", reject);
     });
+    let tmpShapeTerm: Term[] = shapeStore.getQuads(null, df.namedNode('https://w3id.org/tree#shape'), null).map((quad) => quad.object);
+    if (tmpShapeTerm[0]) {
+        shapeTerm = tmpShapeTerm[0];
+        iri = shapeTerm.value;
+    }
+    if (tmpShapeTerm[0] && tmpShapeTerm[0].termType==='NamedNode') {
+        //Dereference the shape and add it here. The iri is not this IRI
+        console.error('GET ' + shapeTerm.value);
+        //Try to dereference this one as well. If it works, nice, if it doesn’t, too bad, we’ll continue without notice.
+        let readStream2 = (
+            await rdfDereference.dereference(shapeTerm.value, {
+              localFiles: true,
+            })
+        ).data;
+        await new Promise((resolve, reject) => {
+            shapeStore.import(readStream2).on("end", resolve).on("error", () => {
+                console.error('Warning: couldn’t fetch ' + iri + ' but continuing');
+                resolve(null);
+            });
+        });
+    }
 
     shapesGraph = new ShapesGraph(shapeStore);
-    const actualMermaid = shapesGraph.toMermaid(df.namedNode(iri));
+    const actualMermaid = shapesGraph.toMermaid(shapeTerm);
     console.log(actualMermaid);
 }
 main();
