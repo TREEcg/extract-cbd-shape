4cef971add5a9a4a61b9ff5e888169b69379397a
Pieter Colpaert <pieter.colpaert@ugent.be>
2024-11-29 12:29:16 +0100

    Fixed MRG test: data changed

--- tests/02 - marine regions LDES/mrg.test.ts
+++ tests/02 - marine regions LDES/mrg.test.ts
@@ -46,34 +46,4 @@ describe("Check whether a member from the MRG source can be fully extracted", fu
       2,
     ); // Test whether it actually did a call
   });
-
-  it("Extract a shape from MRG, based on the blank node and check whether it successfully did a call to a geometry it needs", async () => {
-    let dataStore = RdfStore.createDefault();
-    let readStream2 = (
-      await rdfDereference.dereference(
-        "./tests/02 - marine regions LDES/data.ttl",
-        { localFiles: true },
-      )
-    ).data;
-    await new Promise((resolve, reject) => {
-      dataStore.import(readStream2).on("end", resolve).on("error", reject);
-    });
-    let extractor = new CBDShapeExtractor(dataStore);
-
-    const shapeId = dataStore.getQuads(null, TREE.terms.shape, null)[0].object;
-
-    const id = df.namedNode(
-      "http://marineregions.org/mrgid/24983?t=1690208097",
-    );
-    let result = await extractor.extract(dataStore, id, shapeId);
-
-    const predicates = result
-      .filter((q) => q.subject.equals(id))
-      .map((q) => q.predicate);
-
-    assert.equal(
-      predicates.length,
-      13
-    )
-  });
 });
